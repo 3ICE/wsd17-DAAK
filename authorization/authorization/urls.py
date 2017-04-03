281ec8c4fd5f83f2d86f61a428ac447e0e8f2b4b
Aparajita Chowdhury <chowdhua@student.tut.fi>
2017-04-03 15:52:53 +0300

    login and logout

--- authorization/authorization/urls.py
+++ authorization/authorization/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""authorization URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
