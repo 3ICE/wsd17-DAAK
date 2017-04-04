218a6c2a6bf356197691b8ad33bf1c8fc2cf2536
Aparajita Chowdhury <chowdhua@student.tut.fi>
2017-04-04 10:01:10 +0300

    simple pull

--- DAAK-Store/gettingstarted/urls.py
+++ DAAK-Store/gettingstarted/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
 ]
